416eb0fce4eee1d6ef00b2fabb92c51ce6664e86
saken rai <saken.rai.34@gmail.com>
2022-04-16 18:43:49 +0545

    heroku django api added

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000/api/";
+const baseURL = "https://django-posts.herokuapp.com/api/";
+
+// const baseURL = "http://127.0.0.1:8000/api/";
 
 const api = axios.create({
   baseURL: baseURL,
